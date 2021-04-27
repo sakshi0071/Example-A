c75c5b3a1157217978f4ae3a54057b1f67b8b24d
sakshi735 <tso.sakshi@orbitbiotech.com>
2021-04-27 03:28:32 -0700

    hey

---
+++ practice A2.R
@@ -0,0 +1,42 @@
+#factors
+b= (c("A","B","C","D"))
+b
+b= as.factor(c("A","B","C","D"))
+b
+a=factor(c("low cell count",a"high cell count","moderate cell count"))
+a
+a=ordered= TRUE
+a
+a
+as.numeric(a)
+as.character(a)
+a= factor(c("lowcell c ount","high cell count"," moderate cell count"))
+
+a=levels
+a
+a= factor(c("lowcell c ount","high cell count"," moderate cell count"))
+levels= (c("low cell count","moderate cell count","high cell count"))
+a  
+levels= (c("low cell count","moderate cell count","high cell count"),ordered=TRUE)
+a
+a=levels=ordered=TRUE
+a
+levels= (c("low cell count","moderate cell count","high cell count"))
+a= factor(c("low cell count","high cell count"," moderate cell count"),
+          levels= (c("low cell count","moderate cell count","high cell count"),ordered= TRUE)
+a=factor(c("high BP","low BP","normal BP"))          
+ a=levels(c("high BP","low BP","normal BP"))         
+a
+a 
+a=factor(c("high BP","low BP","normal BP"))      
+a=levels(c("high BP","low BP","Normal BP"))
+as.numeric(a)
+as.character(a)
+a= factor(c("early","late","ontime"))
+a=levels(c("early","ontime","late"),ordered=TRUE)
+A
+a
+a=levels=ordered=TRUE
+as.numeric(a)
+as.character(a)
+a= factor(c("early","late","ontime"))
